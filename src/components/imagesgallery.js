df1deacf452d07496c0d71ee07bc156fd018251f
Marcin Smoła <Marcinure1@gmail.com>
2020-09-29 11:44:02 +0200

    Update imagesgallery.js

--- src/components/imagesgallery.js
+++ src/components/imagesgallery.js
@@ -8,23 +8,33 @@ import StyledRow from "../components/row"
 const GalleryWrapper = styled.div`
     width: 100%;
     display: flex;
-    flex-wrap: wrap;
+    flex-direction: column;
 `
 
 const GalleryImg = styled(Img)`
     width: 100%;
     height: 100%;
     object-fit: contain;
+    z-index: -1;
+    position: relative;
+    &::after {
+        content: '${props => props.label}';
+        color: black;
+        display: block;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
 `
 const ImageLink = styled.a`
     flex-grow: ${props => props.flexGrow || 1};
     object-fit: contain;
-    min-width: 300px;
-    margin: 4px;
+    min-width: 20vw;
+    margin: 8px;
 `
 const LinkedImg = (props) => (
     <ImageLink href={props.fluid.src} target="_blank">
-        <GalleryImg fadeIn {...props}>
+        <GalleryImg fadeIn {...props} >
         </GalleryImg>
     </ImageLink>
 )
@@ -71,12 +81,57 @@ const ImagesGallery = () => {
                   }
               }
           }
+          pies1: file(relativePath: { eq: "2020/pies1.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+          pies2: file(relativePath: { eq: "2020/pies2.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+          pies3: file(relativePath: { eq: "2020/pies3.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+          pies4: file(relativePath: { eq: "2020/pies4.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+          pies5: file(relativePath: { eq: "2020/pies5.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
         }
     `)
     return (
         <GalleryWrapper>
             <GallerySection>
-                <h2>Galeria</h2>
+                <h2>Nasze szczeniaki</h2>
+                <FlexWrapper>
+                    <LinkedImg fluid={data.pies1.childImageSharp.fluid} label="Piesek"/>
+                    <LinkedImg fluid={data.pies2.childImageSharp.fluid} label="Drugi Piesek"/>
+                    <LinkedImg fluid={data.pies3.childImageSharp.fluid} label="Trzeci Piesek"/>
+                    <LinkedImg fluid={data.pies4.childImageSharp.fluid} label="Czwarty Piesek"/>
+                    <LinkedImg fluid={data.pies5.childImageSharp.fluid} label="Piąty Piesek"/>
+                </FlexWrapper>
+            </GallerySection>
+            <GallerySection>
+                <h2>Pełna Galeria</h2>
                 <FlexWrapper>
                     {data.allImageSharp.edges.map((image) => (
                         <LinkedImg fluid={image.node.fluid} key={image.node.fluid.src}/>
